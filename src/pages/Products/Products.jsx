10716ab83d29a98565f270f6cf8d0ec4ad59e62f
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-08-15 14:50:32 +0600

    card design complete

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,19 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { IoGridSharp } from "react-icons/io5";
-import { GiHamburgerMenu } from "react-icons/gi";
 import 'animate.css';
 import axios from 'axios';
 import baseUrl from '../../services/helper';
 import { Helmet } from 'react-helmet';
 
 const Products = () => {
-    const [GridActive, setGridActive] = useState(true)
-    const [TableActive, setTableActive] = useState(false)
     const [Products, setProducts] = useState([])
     const [count, SetCount] = useState(0)
-    const [itemsPerPage, setItemsPerPage] = useState(8)
+    const [itemsPerPage] = useState(10)
     const [currentPage, setCurrentPage] = useState(1)
     const [search, setSearch] = useState('')
     const [loadingData, setLoadingData] = useState(true)
@@ -41,7 +37,7 @@ const Products = () => {
     }
 
     useEffect(() => {
-       
+
         axios.get(`${baseUrl}/api/products?page=${currentPage - 1}&size=${itemsPerPage}&search=${search}`)
             .then(res => {
                 setProducts(res.data)
@@ -67,9 +63,9 @@ const Products = () => {
         setTableActive(true)
     }
 
-    
+
     if (loadingData) {
-        return( <div className="flex justify-center"><span className="loading loading-ring loading-xs"></span>
+        return (<div className="flex justify-center"><span className="loading loading-ring loading-xs"></span>
             <span className="loading loading-ring loading-sm"></span>
             <span className="loading loading-ring loading-md"></span>
             <span className="loading loading-ring loading-lg"></span></div>)
@@ -80,11 +76,9 @@ const Products = () => {
             <Helmet>
                 <title>Estarch | all products</title>
             </Helmet>
-            <h1 className='text-center my-12 bg-base-200 text-3xl py-4 font-bold'>All Products</h1>
+            {/* <h1 className='text-center my-12 bg-base-200 text-3xl py-4 font-bold'>All Products</h1> */}
             <div className='flex my-4 gap-4 flex-wrap'>
 
-                <button onClick={handleGrid} className='btn text-xl'><IoGridSharp /></button>
-                <button onClick={handleTable} className='btn text-xl'><GiHamburgerMenu /></button>
                 <form className='flex gap-1' onSubmit={handleSearch}>
                     <label className="input input-bordered flex items-center gap-2">
                         <input name='search' type="text" className="grow" placeholder="Search" />
@@ -93,73 +87,28 @@ const Products = () => {
 
                 </form>
             </div>
-            <div className={`md:grid-cols-2 gap-6 ${GridActive ? 'grid' : 'hidden'}`}>
+            <div className={`grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-6`}>
                 {
-                    Products.map(p => <div key={p._id} className="flex card shadow-lg animate__animated animate__zoomIn">
-                        <div className="hero-content flex-col lg:flex-row">
-                            <img src={p.image} className="max-w-[270px] rounded-lg shadow-2xl" />
+                    Products.map(p => <div key={p._id} className="animate__animated animate__zoomIn shadow-lg h-[400px] md:h-[540px] relative">
+                        <div className="">
+                            <img src={p.productImage} className="" />
                             <div>
-                                <h1 className="text-xl font-bold pb-4 text-[#7077A1]">{p.title}</h1>
-                                <div>
-                                    <p className='text-[17px]'><span className='font-bold'>Deadline:</span> {p.deadline}</p>
-                                    <p className='text-[17px]'><span className='font-bold'>Location:</span> {p.location}</p>
-                                    <Link to={`/details/${p._id}`}><button className='text-[18px] text-white bg-[#424769] px-3 py-1 mt-2 rounded-md'>View Details</button></Link>
-                                </div>
+                                <h2 className="md:text-[16px] text-[12px] font-bold text-center px-2">
+                                    {p.productName.length > 25
+                                        ? `${p.productName.slice(0, 25)}...`
+                                        : p.productName
+                                    }</h2>
+
+                                    <p className='text-center font-mono font-semibold py-1 absolute bottom-14 md:left-16 left-7'>Tk. {p.price}</p>
                             </div>
+                        </div>
+                        <div className='text-center absolute bottom-4 left-7 md:left-[65px]'>
+                            <button className='btn btn-sm shadow-lg'>BUY NOW</button>
                         </div>
                     </div>)
                 }
             </div>
 
-            <div className={`${TableActive ? 'grid' : 'hidden'}`}>
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th>Image</th>
-                                <th>Title</th>
-                                <th>Category</th>
-                                <th>Location</th>
-                                <th>Deadline</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* row 1 */}
-                            {
-                                Products.map((p) =>
-                                    <tr className='animate__animated animate__zoomIn' key={p._id}>
-                                        <td>
-                                            <div className="flex items-center gap-3">
-                                                <div className="avatar">
-                                                    <div className="mask mask-squircle w-12 h-12">
-                                                        <img src={p.image} alt="Avatar Tailwind CSS Component" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div className="font-bold text-[#7077A1]">{p.title}</div>
-                                        </td>
-                                        <td>
-                                            {p.category}
-                                        </td>
-                                        <td>
-                                            {p.location}
-                                        </td>
-                                        <td>{p.deadline}</td>
-                                        <th>
-                                            <Link to={`/details/${p._id}`}> <button className="btn text-white bg-[#424769]">View Details</button></Link>
-                                        </th>
-                                    </tr>
-                                )
-                            }
-
-                        </tbody>
-                    </table>
-                </div>
-            </div>
             {/* pagination */}
             <div className='flex justify-center mt-12 gap-4'>
                 <button onClick={handlePrev} className="btn">Prev</button>
